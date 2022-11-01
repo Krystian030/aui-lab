1c065debb1ae534bff228524fa2b73dbc26fd274
Krystian Jandy <s184589@student.pg.edu.pl>
2022-11-01 11:12:06 +0100

    create empty student project

---
+++ aui-student/src/main/java/com/example/auistudent/AuiStudentApplication.java
@@ -0,0 +1,13 @@
+package com.example.auistudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuiStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuiStudentApplication.class, args);
+	}
+
+}
---
+++ aui-student/src/test/java/com/example/auistudent/AuiStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auistudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuiStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
