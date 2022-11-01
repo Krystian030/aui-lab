19170ddd8d6c7078ead7dd752208330b006f66a1
Krystian Jandy <s184589@student.pg.edu.pl>
2022-11-01 11:01:42 +0100

    create empty gateway project

---
+++ aui-gateway/src/main/java/jandy/krystian/auigateway/AuiGatewayApplication.java
@@ -0,0 +1,13 @@
+package jandy.krystian.auigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuiGatewayApplication.class, args);
+	}
+
+}
